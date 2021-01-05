e6f1d6800f5df5b1b8a5f03fc95cff3bbae9bc0c
Vikil Naik <vikilnaik@Vikils-MacBook-Pro.local>
2021-01-04 23:07:27 -0500

    Can switch between the different options and the sounds will correspond with the correct audio

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ class DrumKit {
 	constructor() {
 		this.pads = document.querySelectorAll(".pad");
 		this.playBtn = document.querySelector(".play");
+		this.currentKick = "./sounds/kick-classic.wav";
+		this.currentSnare = "./sounds/snare-acoustic01.wav";
+		this.currentHihat = "./sounds/hihat-acoustic01.wav";
 		this.kickAudio = document.querySelector(".kick-sound");
 		this.snareAudio = document.querySelector(".snare-sound");
 		this.hihatAudio = document.querySelector(".hihat-sound");
 		this.index = 0;
 		this.bpm = 150;
 		this.isPlaying = null;
+		this.selects = document.querySelectorAll("select");
 	}
-
 	activePad() {
 		this.classList.toggle("active");
 	}
@@ -51,7 +54,6 @@ class DrumKit {
 			}, interval);
 		}
 	}
-
 	updateBtn() {
 		if (!this.isPlaying) {
 			this.playBtn.innerText = "Stop";
@@ -61,9 +63,26 @@ class DrumKit {
 			this.playBtn.classList.remove("active");
 		}
 	}
+	changeSound(e) {
+		const selectionName = e.target.name;
+		const selectionValue = e.target.value;
+		switch (selectionName) {
+			case "kick-select":
+				this.kickAudio.src = selectionValue;
+				break;
+			case "snare-select":
+				this.snareAudio.src = selectionValue;
+				break;
+			case "hihat-select":
+				this.hihatAudio.src = selectionValue;
+				break;
+		}
+	}
 }
 
 const drumKit = new DrumKit();
+
+// Event Listeners
 
 drumKit.pads.forEach((pad) => {
 	pad.addEventListener("click", drumKit.activePad);
@@ -75,4 +94,10 @@ drumKit.pads.forEach((pad) => {
 drumKit.playBtn.addEventListener("click", function () {
 	drumKit.updateBtn();
 	drumKit.start();
+});
+
+drumKit.selects.forEach((select) => {
+	select.addEventListener("change", function (e) {
+		drumKit.changeSound(e);
+	});
 });
